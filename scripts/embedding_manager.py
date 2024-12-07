ef8add3147712c4561262af10cd6c9ca05335ae3
pradanaadn <pradanaadn21@gmail.com>
2024-12-07 15:07:11 +0800

    feat: add vector db retriever

--- scripts/embedding_manager.py
+++ scripts/embedding_manager.py
@@ -5,19 +5,34 @@ from os import path
 from shutil import rmtree
 
 
-
 class EmbeddingManager:
-    def __init__(self, chunks: Document, persist_directory = "db"):
+    def __init__(self, chunks: Document = None, persist_directory="db", embedding=None):
         self.chunks = chunks
         self.persist_directory = persist_directory
         self.vectordb = None
+        self.embedding = (
+            GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
+            if embedding
+            else embedding
+        )
+
+    def get_retriever(self):
+        self.vectordb = Chroma(
+            persist_directory=self.persist_directory, embedding_function=self.embedding
+        )
+        retriever = self.vectordb.as_retriever(
+            search_type="mmr", search_kwargs={"k": 6, "lambda_mult": 0.25}
+        )
+        return retriever
+
     # Method to create and persist embeddings
     def create_and_persist_embeddings(self):
         if path.exists(self.persist_directory):
             rmtree(self.persist_directory)
         # Creating an instance of OpenAIEmbeddings
-        embedding = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
         # Creating an instance of Chroma with the sections and the embeddings
         self.vectordb = Chroma.from_documents(
-            documents=self.chunks, embedding=embedding, persist_directory=self.persist_directory
+            documents=self.chunks,
+            embedding=self.embedding,
+            persist_directory=self.persist_directory,
         )
