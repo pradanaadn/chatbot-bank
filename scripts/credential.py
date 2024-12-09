2e293c71df5b6065065b0f1870bb54fd3e9f479c
pradanaadn <pradanaadn21@gmail.com>
2024-12-09 21:52:59 +0800

    chore: generate vectordb

--- scripts/credential.py
+++ scripts/credential.py
@@ -4,11 +4,11 @@ from os import environ
 GEMINI_API_KEY = st.secrets.LLM_CREDENTIALS.GEMINI_API_KEY
 
 
-# def load_llm_env_key():
-#     logger.info(GEMINI_API_KEY)
-#     if environ["GOOGLE_API_KEY"] is None:
-#         if GEMINI_API_KEY is not None:
-#             environ["GOOGLE_API_KEY"] = GEMINI_API_KEY
-#             logger.success("Success add GEMINI API KEY to environment variable")
-#         else:
-#             logger.error("Can not find any Gemini API Key")
+def load_llm_env_key():
+    logger.info(GEMINI_API_KEY)
+    if environ["GOOGLE_API_KEY"] is None:
+        if GEMINI_API_KEY is not None:
+            environ["GOOGLE_API_KEY"] = GEMINI_API_KEY
+            logger.success("Success add GEMINI API KEY to environment variable")
+        else:
+            logger.error("Can not find any Gemini API Key")
