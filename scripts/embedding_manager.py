5d0d85b65705ec9bf0f96d344360209770b34531
pradanaadn <pradanaadn21@gmail.com>
2024-12-09 11:03:32 +0800

    debug: error streamlit cloud

--- scripts/embedding_manager.py
+++ scripts/embedding_manager.py
@@ -1,3 +1,8 @@
+import sys
+__import__('pysqlite3')
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
 from langchain_chroma import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.schema import Document
